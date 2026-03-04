565cb5694ff27279434d7284f5cf327a971bd53b
suellykarine <95307379+suellykarine@users.noreply.github.com>
2026-03-04 11:23:25 -0300

    fix: validation errors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Document Management API - INMETA')
     .setDescription(
